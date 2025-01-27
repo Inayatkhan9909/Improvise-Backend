90b15cfa8f64b6ddf3caf4ac5f26f82dfef752c4
Inayat Ullah Khan <khan99.inayat@gmail.com>
2025-01-27 20:15:51 +0530

    fix

--- Routes/courseRoutes.ts
+++ Routes/courseRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { isApprovedInstructor } from "../Middlewares/authorize";
-import {BookCourse, CancelUserCourseBooking, createCourse,getAllCourses,getTopCourses, getUserBookedCourses} from '../Controllers/coursesController'
+import { BookCourse, CancelUserCourseBooking, createCourse, getAllCourses, getTopCourses, getUserBookedCourses } from '../Controllers/coursesController'
 import { deleteCourse } from "../Controllers/coursesController";
 import { updateCourse } from "../Controllers/coursesController";
 import { isStudent } from "../Middlewares/authenticate";
@@ -8,13 +8,13 @@ import { isStudent } from "../Middlewares/authenticate";
 const router = express.Router();
 
 router.post("/create-course", isApprovedInstructor, createCourse);
- router.put("/update-course", isApprovedInstructor, updateCourse);
- router.delete("/delete-course/:courseId", isApprovedInstructor, deleteCourse);
- router.get("/getallcourses", getAllCourses);
- router.get("/gettopcourses", getTopCourses);
- router.put('/bookcourse',isStudent,BookCourse);
- router.delete("/cancel-user-coursebooking/:courseId",isStudent,CancelUserCourseBooking);
- router.get("/get-userbooked-courses",isStudent,getUserBookedCourses );
+router.put("/update-course", isApprovedInstructor, updateCourse);
+router.delete("/delete-course/:courseId", isApprovedInstructor, deleteCourse);
+router.get("/getallcourses", getAllCourses);
+router.get("/gettopcourses", getTopCourses);
+router.put('/bookcourse', isStudent, BookCourse);
+router.delete("/cancel-user-coursebooking/:courseId", isStudent, CancelUserCourseBooking);
+router.get("/get-userbooked-courses", isStudent, getUserBookedCourses);
 
 
 
